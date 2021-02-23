66f3b1ee6c69e0ccb2879bbf56bddc58bcc97e12
Noah <coomsy@protonmail.com>
2021-02-22 20:16:10 -0500

    Bugfix .json() doesn't work?

--- main.py
+++ main.py
@@ -39,19 +39,22 @@ def alcohol():
 # START validating exercise
 @app.route('/alcohol/activity/<num>', methods = ['POST'])
 def check_their_work(num):
+    print('Got request for ' + str(num))
     if not request.headers.get('Token') or _token_value not in request.headers.get('Token'):
         return Response(response='{"error":"Wheres my Token mf"}', status=400)
     data = json.loads(open('data/alcohol.json', 'r').read())
     
     try:
-        learner_answer = request.json()
-    except:
+        learner_answer = json.loads(request.data.decode())
+        print(learner_answer)
+    except Exception as ex:
+        print(repr(ex))
         # no idea what error is, just tell em they didn't format correctly
-        return Response(response='{"error": "What did you just send me?... this aint json?"', 
+        return Response(response='{"error": "What did you just send me?... this aint json?"}', 
                         status=400)
     
     if not learner_answer.get('answer'):
-        return Response(response='{"error": "Could not find key \"answer\"... see / page"', 
+        return Response(response='{"error": "Could not find key \"answer\"... see / page"}', 
                         status=400)
 
     learner_answer = learner_answer['answer']
@@ -60,21 +63,21 @@ def check_their_work(num):
     #! no this isn't how I write stuff, this app is for fun. So I try to do one liners 
     #! + keep the learners from trying to cheat. Cuz aint no way they doing these
     # !!! IMPORTANT !!!
-
+    answer = ''
     # Most expensive item (name)
-    if num == 1:
+    if num == '1':
         #str
         answer = list(max([ item for _, item in data.items() ], key=lambda x: list(x.values())[0]))[0]
     # Least expensive item (name)
-    elif num == 2:
+    elif num == '2':
         #str
         answer = list(min([ item for _, item in data.items() ], key=lambda x: list(x.values())[0]))[0]
     # Top 10% Most Expensive (names)
-    elif num == 3:
+    elif num == '3':
         # [ {str,float},... ]
         answer = list(sorted([ item for _, item in data.items() ], key=lambda x: list(x.values())[0], reverse=True))[:int(len(data) * .1 )]
     # Removing all special chars, in names
-    elif num == 4:
+    elif num == '4':
         answer = []
         for _, item in data.items():
             for char in string.punctuation:
